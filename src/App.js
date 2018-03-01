ebaf1341377b857f1f998d4775228f09da63e3a4
Anita Ilieva <anita.ilieva@acando.no>
2018-03-01 15:39:29 +0100

    Add actioncreator and loginform

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+	componentWillMount() {
+	const config = {
+    apiKey: 'AIzaSyBFFgX5qX0onF1KiUk2Qcf3WOpzG6ykZiQ',
+    authDomain: 'manager-10e2f.firebaseapp.com',
+    databaseURL: 'https://manager-10e2f.firebaseio.com',
+    projectId: 'manager-10e2f',
+    storageBucket: 'manager-10e2f.appspot.com',
+    messagingSenderId: '959608893903'
+  };
+  firebase.initializeApp(config);
+}
+
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>
-						Hello
-					</Text>
-				</View>
+					<LoginForm />
 			</Provider>
 		);
 	}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Input, Button } from './common';
+import * as actions from './actions';
+
+class LoginForm extends Component {
+
+	onEmailChange = (text) => {
+
+	}
+
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Input 
+					label="Email"
+					placeholder="email@email.com" 
+					onChangeText={this.onEmailChange}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+					secureTextEntry
+					label="Password"
+					placeholder="password"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Login
+					</Button>
+				</CardSection>
+				
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
