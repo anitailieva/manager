082e8c47262412d73fa80d8c267c7fcd36627224
Anita Ilieva <anita.ilieva@acando.no>
2018-03-03 11:06:45 +0100

    Add redux thunk

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { connect } from 'react-redux';
 
 class LoginForm extends Component {
@@ -12,6 +12,12 @@ class LoginForm extends Component {
 	onPasswordChange = (text) => {
 		this.props.passwordChanged(text);
 	}
+
+	onButtonPress = () => {
+		const { email, password } = this.props;
+		this.props.loginUser( { email, password });
+	}
+
 
 	render() {
 		return (
@@ -36,7 +42,7 @@ class LoginForm extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress}>
 						Login
 					</Button>
 				</CardSection>			
@@ -54,5 +60,6 @@ const mapStateToProps = state => {
 };
 
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, { 
+	emailChanged, passwordChanged, loginUser })(LoginForm);
 
