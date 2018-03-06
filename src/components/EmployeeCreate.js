c269f5e45bffd9eeff72531490838057c74a3479
Anita Ilieva <anita.ilieva@acando.no>
2018-03-06 11:43:51 +0100

    Add Picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from 	'../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -26,7 +27,21 @@ class EmployeeCreate extends Component {
 					/>
 				</CardSection>
 
-				<CardSection>
+				<CardSection style={{ flexDirection: 'column' }}>
+					<Text style={styles.pickerLabelStyle}>Shift</Text>
+					<Picker
+						style={{ flex: 1 }}
+						selectedValue={this.props.shift}
+						onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+					>
+						<Picker.Item label="Monday" value="Monday" />
+						<Picker.Item label="Tuesday" value="Tuesday" />
+						<Picker.Item label="Wednesday" value="Wednesday" />
+						<Picker.Item label="Thursday" value="Thursday" />
+						<Picker.Item label="Friday" value="Friday" />
+						<Picker.Item label="Saturday" value="Saturday" />
+						<Picker.Item label="Sunday" value="Sunday" />
+					</Picker>
 				</CardSection>
 
 				<CardSection>
@@ -39,11 +54,17 @@ class EmployeeCreate extends Component {
 	}
 }
 
+const styles = {
+	pickerLabelStyle: {
+		fontSize: 18,
+		paddingLeft: 20
+	}
+};
+
 const mapStateToProps = (state) => {
 	const { name, phone, shift } = state.employeeForm;
 
 	return { name, phone, shift };
 };
-
 
 export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
