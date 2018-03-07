f5053daf6a63081a0789c284b49615b107a94228
Anita Ilieva <anita.ilieva@acando.no>
2018-03-07 13:41:46 +0100

    Manage to fetch data from firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
-
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 export const employeeUpdate = ({ prop, value }) => {
 	return {
 		type: EMPLOYEE_UPDATE,
@@ -18,7 +17,18 @@ export const employeeCreate = ({ name, phone, shift }) => {
 		.push({ name, phone, shift })
 		.then(() => {
 			dispatch({ type: EMPLOYEE_CREATE });
-			Actions.employeeList({ type: 'reset' });
+			Actions.employeeList();
+		});
+	};
+};
+
+export const employeesFetch = () => {
+	const { currentUser } = firebase.auth();
+
+	return (dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees`)
+			.on('value', snapshot => {
+				dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
 		});
 	};
 };
